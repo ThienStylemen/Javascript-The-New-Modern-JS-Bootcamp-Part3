179e2508117308122e3bd26c42d4a8159f512c6b
ThienStylemen <thienstylemen@gmail.com>
2020-03-29 01:59:13 +0700

    Parsing Form Data

--- ecomm/index.js
+++ ecomm/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 //(property) Application.get: ((name: string) => any) & IRouterMatcher<Express, any>
 app.get('/', (req, res) => {
-    res.send(`
+  res.send(`
       <div>
         <form method="POST">
           <input name="email" placeholder="email" />
@@ -13,14 +13,25 @@ app.get('/', (req, res) => {
         </form>
       </div>
     `);
-  }); 
-
-app.post('/', (req,res)=>{
-    res.send('Account created');
 });
-  
+
+app.post('/', (req, res) => {
+  // get access to email, password, passwordConfirmation 
+  //(method) internal.Readable.on(event: "close", listener: () => void): Request<ParamsDictionary, any, any> (+5 overloads)
+  req.on('data', data =>{   // .on ~= .addEventListener
+    const parsed = data.toString('utf8').split('&');
+    const formData = {};
+    for(let pair of parsed){
+      const [key, value] = pair.split('=');  // destructure, first part before = is key, second part is value
+      formData[key] = value;  
+    }
+    console.log(formData);
+  });
+  res.send('Account created');
+});
+
 
 //method) Application.listen(port: number, hostname: string, backlog: number, callback?: (...args: any[]) => void): Server (+5 overloads)
-app.listen(3000, ()=>{
-    console.log('listening');
+app.listen(3000, () => {
+  console.log('listening');
 })
