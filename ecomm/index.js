1c3ed00f9fc3766f9032a7324a1a24fc8acefc7b
ThienStylemen <thienstylemen@gmail.com>
2020-03-29 00:34:57 +0700

    Understanding Form

--- ecomm/index.js
+++ ecomm/index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const app = express();
 
 //(property) Application.get: ((name: string) => any) & IRouterMatcher<Express, any>
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send(`
-        <div>
-            <form>
-                <input placeholder="email" />
-                <input placeholder="password""/>
-                <input placeholder="password confirmation""/>
-                <button> Sign Up </button>
-            </form>
-        </div>
+      <div>
+        <form method="POST">
+          <input name="email" placeholder="email" />
+          <input name="password" placeholder="password" />
+          <input name="passwordConfirmation" placeholder="password confirmation" />
+          <button>Sign Up</button>
+        </form>
+      </div>
     `);
+  }); 
+
+app.post('/', (req,res)=>{
+    res.send('Account created');
 });
+  
 
 //method) Application.listen(port: number, hostname: string, backlog: number, callback?: (...args: any[]) => void): Server (+5 overloads)
 app.listen(3000, ()=>{
