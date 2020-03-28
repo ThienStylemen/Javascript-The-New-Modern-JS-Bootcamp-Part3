3e61b7c874b5b3d926b921cfecc4240519fa124d
ThienStylemen <thienstylemen@gmail.com>
2020-03-29 00:10:28 +0700

    Displaying Simple HTML

--- ecomm/index.js
+++ ecomm/index.js
@@ -3,7 +3,16 @@ const app = express();
 
 //(property) Application.get: ((name: string) => any) & IRouterMatcher<Express, any>
 app.get('/', (req,res)=>{
-    res.send('Hi');
+    res.send(`
+        <div>
+            <form>
+                <input placeholder="email" />
+                <input placeholder="password""/>
+                <input placeholder="password confirmation""/>
+                <button> Sign Up </button>
+            </form>
+        </div>
+    `);
 });
 
 //method) Application.listen(port: number, hostname: string, backlog: number, callback?: (...args: any[]) => void): Server (+5 overloads)
