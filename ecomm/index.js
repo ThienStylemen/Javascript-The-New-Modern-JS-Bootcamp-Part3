9e3f8d21b541fd0993404d8e9ac8c98b4c2f7e08
ThienStylemen <thienstylemen@gmail.com>
2020-03-28 23:35:28 +0700

    Creating a Web Server

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,1 +1,12 @@
-console.log('hi there');
+const express = require('express');
+const app = express();
+
+//(property) Application.get: ((name: string) => any) & IRouterMatcher<Express, any>
+app.get('/', (req,res)=>{
+    res.send('Hi');
+});
+
+//method) Application.listen(port: number, hostname: string, backlog: number, callback?: (...args: any[]) => void): Server (+5 overloads)
+app.listen(3000, ()=>{
+    console.log('listening');
+})
