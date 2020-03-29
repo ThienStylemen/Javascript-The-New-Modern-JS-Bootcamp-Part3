1d98a1f0ac114cc4a4ae739b2a65b1006f28f074
ThienStylemen <thienstylemen@gmail.com>
2020-03-30 02:46:05 +0700

    Fetching a Session

--- ecomm/index.js
+++ ecomm/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const usersRepo = require('./repositories/user');
+const cookieSession = require('cookie-session');
 
 //(property) Application.use: ApplicationRequestHandler<Express>
 app.use(bodyParser.urlencoded({extended: true})); //  midlewares automatically instead of ... app.post('/products', bodyParser.urlencoded({extended: true})  .......
-
+app.use(cookieSession({keys: ['asdadasd']})); //exactly one property, random characters is essentially the encryption key that is going to be used to encrypt all that data
 //(property) Application.get: ((name: string) => any) & IRouterMatcher<Express, any>
 app.get('/', (req, res) => {
   res.send(`
       <div>
+        YOUR ID is: ${req.session.userId}
         <form method="POST">
           <input name="email" placeholder="email" />
           <input name="password" placeholder="password" />
@@ -30,10 +32,10 @@ app.post('/', async (req, res) => {
   if (password !== passwordConfirmation) return res.send('Passwords must match');
 
   // create a user in our user repo to represent this person
-  const user = await usersRepo.create({ email, password}); //{email: email, password: password}
+  const user = await usersRepo.create({ email, password}); //{email: email, password: password}, we also got the id
   
   // store the id inside the user cookie, now we use third pakage
-
+  req.session.userId = user.id // added by cookie session req.session === {}
 
   res.send('Account created');
 });
