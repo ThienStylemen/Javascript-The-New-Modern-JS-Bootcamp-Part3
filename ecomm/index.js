76cf46943d3303ec974124ae19de148a569fc259
ThienStylemen <thienstylemen@gmail.com>
2020-03-29 23:15:51 +0700

    Signup Validation Logic

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/user');
 
 //(property) Application.use: ApplicationRequestHandler<Express>
 app.use(bodyParser.urlencoded({extended: true})); //  midlewares automatically instead of ... app.post('/products', bodyParser.urlencoded({extended: true})  .......
@@ -21,12 +22,15 @@ app.get('/', (req, res) => {
 
 //function bodyParser.urlencoded(options?: bodyParser.OptionsUrlencoded): createServer.NextHandleFunction
 //Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
-app.post('/', (req, res) => { 
-  console.log(req.body); //req.bodyy cung dc
-  //console.log(req); // here we checking the method is 'POST'
+app.post('/', async (req, res) => { 
+  // console.log(req.body); //req.bodyy cung dc
+  const {email, password, passwordConfirmation} = req.body;
+  const existingUser = await usersRepo.getOneBy({email});
+  if (existingUser) return res.send('Email in use');
+  if (password !== passwordConfirmation) return res.send('Passwords must match');
+
   res.send('Account created');
 });
-
 
 //method) Application.listen(port: number, hostname: string, backlog: number, callback?: (...args: any[]) => void): Server (+5 overloads)
 app.listen(3000, () => {
