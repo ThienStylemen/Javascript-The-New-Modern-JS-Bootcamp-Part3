615b167e4055bbf0b589b90f48b19e31d8e0e6c6
ThienStylemen <thienstylemen@gmail.com>
2020-03-31 22:57:53 +0700

    Creating the Products Repository

---
+++ ecomm/repositories/products.js
@@ -0,0 +1,6 @@
+const Repository = require('./repository');
+
+class ProductsRepository extends Repository{ }
+
+//create an instance of ProductsRepository and exports its
+module.exports = new ProductsRepository;
--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const ProductsRepo = require('../../repositories/products');
 
 router.get('/admin/products', (req, res) => {
-
+    
 });
 
 router.get('/admin/products/new', (req,res)=>{
