0ced9ca80b252222603434fef449752202e72ede
ThienStylemen <thienstylemen@gmail.com>
2020-03-31 21:45:03 +0700

    Product Routes

--- ecomm/index.js
+++ ecomm/index.js
@@ -3,13 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 app.use(express.static('public'));  // this is what we mandatory to run main.css
 //(property) Application.use: ApplicationRequestHandler<Express>
 app.use(bodyParser.urlencoded({extended: true})); //  midlewares automatically instead of ... app.post('/products', bodyParser.urlencoded({extended: true})  .......
 app.use(cookieSession({keys: ['asdadasd']})); //exactly one property, random characters is essentially the encryption key that is going to be used to encrypt all that data
 app.use(authRouter);
-
+app.use(productsRouter);
 //method) Application.listen(port: number, hostname: string, backlog: number, callback?: (...args: any[]) => void): Server (+5 overloads)
 app.listen(3000, () => {
   console.log('listening');
