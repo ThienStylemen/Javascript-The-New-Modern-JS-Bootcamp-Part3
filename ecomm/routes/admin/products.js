53124ae4e2011215e1faa6aafe90006ab09061f7
ThienStylemen <thienstylemen@gmail.com>
2020-04-01 03:04:56 +0700

    Accessing the Uploaded File

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
+
 const ProductsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({storage: multer.memoryStorage()});//multer(options?: multer.Options): Multer. Returns a Multer instance that provides several methods for generating middleware that process files uploaded in multipart/form-data format.
+
 router.get('/admin/products', (req, res) => { });
 
 router.get('/admin/products/new', (req, res) => {  //.get is to send a form
@@ -17,9 +22,10 @@ router.post(
         requireTitle,
         requirePrice 
     ],
+    upload.single('image'), //in htlm : name="image"
     (req, res) => { 
         const errors = validationResult(req);
-        console.log(req.body);
+        console.log(req.file);
         res.send('SUBMITTED');
     }
 );
