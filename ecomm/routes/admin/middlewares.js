971cf2dabd917898b9d62abccc4ed6b56e2e741a
ThienStylemen <thienstylemen@gmail.com>
2020-04-02 02:33:48 +0700

    Requiring Authentication

--- ecomm/routes/admin/middlewares.js
+++ ecomm/routes/admin/middlewares.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');// const expressValidator = require()... then  expressValidator.check() may be annoyer
 module.exports = {
+
     handleErrors(templateFunc){//thien: templateFunc will be runed at some point in this function, not immediately
         return (req,res, next)=>{
             const errors = validationResult(req);   // array of error of check() func
@@ -8,5 +9,12 @@ module.exports = {
             }
             next();
         }
+    },
+
+    requireAuth(req,res,next){// arguments like this because there is no customisation required like handleErrors func
+        if(!req.session.userId){
+            return res.redirect('/signin');
+        }
+        next();
     }
 }
--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 
-const {handleErrors} = require('./middlewares');    // helper func
+const {handleErrors, requireAuth} = require('./middlewares');    // helper func
 
 const ProductsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
@@ -11,22 +11,25 @@ const { requireTitle, requirePrice } = require('./validators');
 const router = express.Router();
 const upload = multer({storage: multer.memoryStorage()});//multer(options?: multer.Options): Multer. Returns a Multer instance that provides several methods for generating middleware that process files uploaded in multipart/form-data format.
 
-router.get('/admin/products', async (req, res) => { 
+router.get('/admin/products',requireAuth,async (req, res) => { 
     const products = await ProductsRepo.getAll();
     res.send(productsIndexTemplate({products}));
 });
 
-router.get('/admin/products/new', (req, res) => {  //.get is to send a form
+router.get('/admin/products/new',requireAuth ,(req, res) => {  //.get is to send a form
+   
     res.send(productsNewTemplate({}));
 });
 
 router.post(
     '/admin/products/new', 
+    requireAuth,    // signin before upload.single()
     //also going to parse all the different text field (req.body)
     upload.single('image'), //in htlm, name="image", //now that we're doing this image upload we have a very different encoding type on our form and so the line "app.use(bodyParser.urlencoded({extended: true}));" no longer applied
     [requireTitle, requirePrice],   // if upload.single stay below this line, Nothing is parsed 
     handleErrors(productsNewTemplate),//There are no parentheses because we are passing a reference that function that can be called at some point time in the future repeatedly
     async (req, res) => { 
+        
         const image = req.file.buffer.toString('base64');// base64 can safely represent an image in the a string format
         const {title, price} = req.body;//upload.single('image')
         await ProductsRepo.create({title, price, image});
