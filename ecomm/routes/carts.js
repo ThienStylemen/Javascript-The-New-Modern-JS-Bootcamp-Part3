0e6caabc1ddd6c140827d3d9e5c60929005516dc
ThienStylemen <thienstylemen@gmail.com>
2020-04-04 03:46:19 +0700

    Creating a Cart, One Way or Another

--- ecomm/routes/carts.js
+++ ecomm/routes/carts.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 const router = express.Router();
 
 //Receive a post request to add an item to a cart
-router.post('/cart/products', (req,res)=>{
-    console.log(req.body.productId); 
+router.post('/cart/products', async (req,res)=>{
+    // Figure out the cart
+    let cart;
+    if(!req.session.cartId){ //whether the user already has cartId stored inside of our cars Repository
+        // we dont have a cart, so need to create one, store the cart id on the req.session.cartId, 
+        cart = await cartsRepo.create({items: []}); // create o day cung se tao ra 1 random id
+        req.session.cartId = cart.id;
+    }else{
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+    console.log(cart);
+
+    // either increment quantity for existing product or add new product to items array
     res.send('Product Added to cart');
 });
 
